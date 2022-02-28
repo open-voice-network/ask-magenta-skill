e6b1df2dd58c2aa73f16880b020a73c858e5a449
Vadim Tarasevic <vadim@based.at>
2022-02-28 18:38:06 +0100

    Auto-translate to German and back
    Signed-off-by: Vadim Tarasevic <vadim@based.at>

--- __init__.py
+++ __init__.py
@@ -1,24 +1,34 @@
 from mycroft import MycroftSkill, intent_handler
 from mycroft.messagebus.message import Message
 from mycroft.util import LOG
+from adapt.intent import IntentBuilder
+from mtranslate import translate
 
 from .magenta.voice.client import ApiClient
+
+LANG = "de"
+NO_INTENT = "no.intent"
+ASK_MAGENTA = "ask.magenta"
 
 
 class AskMagenta(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_handler("magenta.ask.intent")
+    @intent_handler(IntentBuilder("AskMagentaIntent").require("AskMagentaKeyword").require("utterance").build())
     def handle_magenta_ask(self, message: Message):
-        LOG.info("Message: %s", repr(message))
+        LOG.info("Message: %s", message.serialize())
         utterance = message.data["utterance"]
-        self.speak_dialog("magenta.ask", message.data)
+        translated = translate(utterance, from_language=self.lang, to_language=LANG)
+        self.speak_dialog(ASK_MAGENTA, message.data)
         with ApiClient() as client:
             client.connect()
-            response = client.invoke_text(utterance)
+            LOG.info("Translated: %s", translated)
+            response = client.invoke_text(translated)
             LOG.info("Response: %s", repr(response))
-            self.speak_dialog(response.text)
+            answer = translate(response.text, from_language=LANG, to_language=self.lang) if response.text else NO_INTENT
+            LOG.info("Translated answer: %s", answer)
+            self.speak_dialog(answer)
 
 
 def create_skill():
